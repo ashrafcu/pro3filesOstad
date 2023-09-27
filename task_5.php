91f93ae317146d816c71233172b5c46605c4f854
Ashraf Ahmed <ahmashraf@gmail.com>
2023-09-27 18:43:07 +0600

    Mistakes in Password String Resolved

--- task_5.php
+++ task_5.php
@@ -1,12 +1,19 @@
 <?php
 function generatePassword($length){
-$chars="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+";
-$charsLength=strlen($chars);
+$onlySmalls="abcdefghijklmnopqrstuvwxyz";
+$onlyCapitals="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+$onlyNumbers="1234567890";
+$specialChars="!@#$%^&*()_+";
+$smallCharsLength=strlen($onlySmalls)-1;
+$capitalCharsLength=strlen($onlyCapitals)-1;
+$noCharsLength=strlen($onlyNumbers)-1;
+$specialCharsLength=strlen($specialChars)-1;
 $password= "";
-for ($i=0; $i<$length; $i++) {
-    $password .= $chars[rand(0,$charsLength)];
+for ($i=0; $i<($length/4); $i++) {
+    $password .= $onlySmalls[rand(0, $smallCharsLength)].$onlyNumbers[rand(0, $noCharsLength)].$onlyCapitals[rand(0,$capitalCharsLength)].$specialChars[rand(0,$specialCharsLength)];
+
 }
-return $password;
+echo $password;
 }
-echo generatePassword(12);
+generatePassword(12);
 ?>
