f864ebbd224ef09aa604ff3af0bc10208cfbe614
Ashraf Ahmed <ahmashraf@gmail.com>
2023-09-27 16:55:44 +0600

    Creating Function for Grade Calculation

--- task_4.php
+++ task_4.php
@@ -1,50 +1,33 @@
 <?php
-$gradingConditions = array(
-    'A+' => 80,
-    'A' => 70,
-    'A-' => 60,
-    'B' => 50,
-    'C' => 40,
-    'D' => 33,
-    'F' => 0
-);
-function findGrade($totalAvgScore, $gradingConditions) {
-    foreach ($gradingConditions as $grade => $minimumScore) {
-        if ($totalAvgScore >= $minimumScore) {
-            return $grade;
-        }
-    }
-    return 'Invalid Result';
-}
-$studentGrades = array(
-    array(
-        'name' => 'Rahad',
-        'math' => 88,
-        'english' => 98,
-        'science' => 81 
-    ),
-    array(
-        'name' => 'Sayem',
-        'math' => 23,
-        'english' => 12,
-        'science' => 11 
-    ),
-    array(
-        'name' => 'Sanjida',
-        'math' => 78,
-        'english' => 52,
-        'science' => 67 
-    )
-);
 
-foreach ($studentGrades as $student) {
-    $name = $student['name'];
-    $mathMarks = $student['math'];
-    $englishMarks= $student['english'];
-    $scienceMarks= $student ['science'];
-    
-    $totalAvgScore = ($mathMarks+$englishMarks+$scienceMarks)/(count($studentGrades[0])-1);
-    $grade = findGrade($totalAvgScore, $gradingConditions);
-    echo "{$name} achieved {$grade} in average with {$mathMarks} in Math, {$englishMarks} in English and {$scienceMarks} in Science."."\n";
+$studentGrades = [
+    'Rahad' => ['math' => 88, 'english' => 98, 'science' => 81],
+    'Sayem' => ['math' => 56, 'english' => 40, 'science' => 33],
+    'Sanjida' => ['math' => 78, 'english' => 52, 'science' => 67]
+];
+
+function getGrades($allStudentGrades)
+{
+    foreach ($allStudentGrades as $student => $scores) {
+        $avgScore = floor(array_sum($scores) / count($scores));
+        if (100 >= $avgScore && 80 <= $avgScore):
+            echo "$student got A+ with " . $avgScore . "% score. \n";
+        elseif (80 > $avgScore && 70 <= $avgScore):
+            echo "$student got A- with " . $avgScore . "% score. \n";
+        elseif (70 > $avgScore && 60 <= $avgScore):
+            echo "$student got A- with " . $avgScore . "% score. \n";
+        elseif (60 > $avgScore && 50 <= $avgScore):
+            echo "$student got B with " . $avgScore . "% score. \n";
+        elseif (50 > $avgScore && 40 <= $avgScore):
+            echo "$student got C with " . $avgScore . "% score. \n";
+        elseif (40 > $avgScore && 33 <= $avgScore):
+            echo "$student got D with " . $avgScore . "% score. \n";
+        elseif (100 < $avgScore):
+            echo "Invalid Score. Please input numbers smaller than 100. \n";
+        else:
+            echo "$student got F with " . $avgScore . "% score. \n";
+        endif;
+    }
 }
+getGrades($studentGrades);
 ?>
